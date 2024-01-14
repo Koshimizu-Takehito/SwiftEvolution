91c7c9be73f42b3293f991d51f54562108c78d9c
Koshimizu-Takehito <takehito.koshimizu@gmail.com>
2024-01-14 16:47:10 +0900

    コメント

--- SwiftEvolutionApp/Sources/Models/Models/Markdown.swift
+++ SwiftEvolutionApp/Sources/Models/Models/Markdown.swift
@@ -29,13 +29,17 @@ private extension Markdown {
     private func fetch() async throws {
         let url = url ?? MarkdownURL(link: proposal.link)
         let (data, _) = try await URLSession.shared.data(from: url.rawValue)
+        // 取得したテキストの特殊文字をエスケープしておく。
+        // エスケープしないと、marked が解析に失敗する。
         markdown = (String(data: data, encoding: .utf8) ?? "")
             .replacingOccurrences(of: "\n", with: #"\n"#)
             .replacingOccurrences(of: "'", with: #"\'"#)
         buildHTML()
     }
 
+    /// github-markdown の　CSS
     var githubMarkdownCss: String {
+        // プロポーザルに関連したレビューステータスの配色をアクセントカラーとして注入
         let (dark, light) = proposal.state.accentColor
         return Assets.CSS.githubMarkdown.asset
             .replacingOccurrences(of: "$color-accent-fg-dark", with: dark)
@@ -44,12 +48,19 @@ private extension Markdown {
 
     func buildHTML() {
         self.html = Assets.HTML.proposalTemplate.asset
+            // HTML のタイトルを設定
             .replacingOccurrences(of: "$title", with: proposal.title)
+            // github-markdown の　CSS を設定
             .replacingOccurrences(of: "$githubMarkdownCss", with: githubMarkdownCss)
+            // シンタックスハイライトの　CSS を設定
             .replacingOccurrences(of: "$highlightjsStyleCss", with: highlight.asset)
+            // marked の javascript を設定。マークダウンから HTML に変換される
             .replacingOccurrences(of: "$markedJs", with: Assets.Js.marked.asset)
+            // シンタックスハイライトの　javascript を設定
             .replacingOccurrences(of: "$highlightJs", with: Assets.Js.highlight.asset)
+            // シンタックスハイライトの　javascript を設定（ Swift ）
             .replacingOccurrences(of: "$highlightJsSwift", with: Assets.Js.highlightSwift.asset)
+            // マークダウンを HTML ファイルに注入
             .replacingOccurrences(of: "$markdown", with: markdown)
     }
 }
