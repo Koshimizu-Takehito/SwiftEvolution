78becc6261bd739818f0e24b3c1a67995ba5ed19
takehito <takehito.koshimizu@gmail.com>
2025-08-17 15:53:58 +0900

    Refactor

--- App/App.swift
+++ App/App.swift
@@ -1,38 +1,16 @@
-import EvolutionModel
 import EvolutionModule
-import EvolutionUI
-import SwiftData
 import SwiftUI
 
 @main
 struct App: SwiftUI.App {
     var body: some Scene {
-        WindowGroup {
-            ContentView().modelContainer(for: ProposalObject.self)
+        AppScene {
+            ContentView()
         }
-        .commands {
-            FilterCommands()
-        }
-        #if os(macOS)
-            Settings {
-                SettingsView()
-            }
-        #endif
     }
 }
 
 #Preview(traits: .proposal) {
-    ContentView().environment(\.colorScheme, .dark)
-}
-
-#Preview("Landscape", traits: .proposal, .landscapeLeft) {
-    ContentView().environment(\.colorScheme, .dark)
-}
-
-#Preview("Assistive access", traits: .proposal, .assistiveAccess) {
-    ContentView().environment(\.colorScheme, .dark)
-}
-
-#Preview("Landscape", traits: .proposal, .landscapeLeft, .assistiveAccess) {
-    ContentView().environment(\.colorScheme, .dark)
+    ContentView()
+        .environment(\.colorScheme, .dark)
 }
